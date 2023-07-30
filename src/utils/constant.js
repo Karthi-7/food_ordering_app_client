1d8547419f9384e9dde782f0b50922d70f5172f4
Karthi-7 <karthikeyancvl96@gmail.com>
2023-07-30 20:01:54 +0530

    backend api added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,5 @@
-export const Swiggy_URL='https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.0168445&lng=76.9558321&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING'
-export const Swiggy_Img_url='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/'
-export const Restaurant_Url='http://localhost:7000/foodApp/api/v1/restaurant'
-export const RestaurantById_Url='http://localhost:7000/foodApp/api/v1/restaurant/'
-export const image_url="http://localhost:7000/"
-export const menu_Restarunt_Url='http://localhost:7000/foodApp/api/v1/foodItems/restaurant/'
+
+export const Restaurant_Url='https://ichirakufoodapp.onrender.com/foodApp/api/v1/restaurant'
+export const RestaurantById_Url='https://ichirakufoodapp.onrender.com/foodApp/api/v1/restaurant/'
+export const image_url="https://ichirakufoodapp.onrender.com/"
+export const menu_Restarunt_Url='https://ichirakufoodapp.onrender.com/foodApp/api/v1/foodItems/restaurant/'
